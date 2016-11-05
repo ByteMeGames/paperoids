ac7a86b686c98ef25fb00af978a9fb86a9f30149
Chris McCabe <chris.mccabe1988@gmail.com>
2016-11-05 11:09:52 +0000

    Removing unused code

--- Source/SketchWars/SketchCharacter.cpp
+++ Source/SketchWars/SketchCharacter.cpp
@@ -64,16 +64,6 @@ void ASketchCharacter::MoveUp(float val) {
 void ASketchCharacter::Fire() {
 	// Attempt to fire a projectile.
 	if (ProjectileClass) {
-		// Get the camera transform.
-		//FVector CameraLocation;
-		//FRotator CameraRotation;
-		//GetActorEyesViewPoint(CameraLocation, CameraRotation);
-
-		// Transform MuzzleOffset from camera space to world space.
-		//FVector MuzzleLocation = CameraLocation + FTransform(CameraRotation).TransformVector(MuzzleOffset);
-		//FRotator MuzzleRotation = CameraRotation;
-		// Skew the aim to be slightly upwards.
-		//MuzzleRotation.Pitch += 10.0f;
 		UWorld* World = GetWorld();
 		if (World) {
 			FActorSpawnParameters SpawnParams;
