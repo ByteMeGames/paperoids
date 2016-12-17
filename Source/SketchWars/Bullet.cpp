9d838a580212e44be4554ac560df3591563f6fe8
dee-mccabe <dee.mccabe@hotmail.co.uk>
2016-12-17 13:44:37 +0000

    Merging

--- Source/SketchWars/Bullet.cpp
+++ Source/SketchWars/Bullet.cpp
@@ -67,6 +67,7 @@ void ABullet::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitive
 		auto Player = Cast<ASketchCharacter>(UGameplayStatics::GetPlayerCharacter(this, 0));
 		auto Asteroid = Cast<AAsteroid>(OtherActor);
 		Player->IncrementScore(Asteroid->ScoreValue);
+		Asteroid->SpawnFragments();
 		Asteroid->Destroy();
 		this->Destroy();
 	}
